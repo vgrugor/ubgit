4d2bad0247b2fd9df8a23fc3868f454c9648613f
vlad.greg <vgrugor@gmail.com>
2020-04-18 14:09:37 +0300

    chore(all): для загрузки на ПК

---
+++ app/Http/Controllers/DivisionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DivisionController extends Controller
+{
+    public function add()
+    {
+        
+    }
+    
+    public function store(Request $request)
+    {
+        
+    }
+}
