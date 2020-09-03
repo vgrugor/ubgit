f8845338ad9373a1539466e23fa48fdbd301f075
vgrugor <vgrugor@gmail.com>
2020-09-03 16:48:23 +0300

    feat(index): отображение необработаных заявок VPN
    На главной странице отображаются недоконца обработанные заявки на
    получения доступа

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Worker;
+
 class IndexController extends Controller
 {
     public function index() {
         
-        return view('index');
+        $workersNoVpn = Worker::leftJoin('positions', 'positions.id', '=', 'workers.position_id')
+                ->leftJoin('vpn_statuses', 'vpn_statuses.id', '=', 'workers.vpn_status_id')
+                ->select('workers.id as worker_id',
+                        'workers.name as worker', 
+                        'workers.note as note',
+                        'workers.vpn_status_id',
+                        'positions.name as position',
+                        'vpn_statuses.name as vpn_status')
+                ->where('workers.vpn_status_id', '<', 5)
+                ->where('workers.vpn_status_id', '>', 1)
+                ->get();
+        
+        return view('index')
+            ->with(['workersNoVpnList' => $workersNoVpn]);
         
     }
     //
